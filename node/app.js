38b26b91d773c214aa55369ae0636ca0b5df88e3
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-11-18 16:35:08 -0800

    add devStart script for local testing via localtunnel

--- node/app.js
+++ node/app.js
@@ -83,6 +83,7 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
+  console.log('got request');
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
@@ -422,7 +423,7 @@ function sendImageMessage(recipientId) {
       }
     }
   };
-
+  console.log(messageData.message.attachment.payload);
   callSendAPI(messageData);
 }
 
---
+++ node/devStart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+tmpFile="start.txt"
+
+lt --port 5000 > $tmpFile &
+
+sleep 2
+
+serverUrl=`grep -oE 'https://.*' $tmpFile`
+echo Found server url: $serverUrl
+
+
+export SERVER_URL=$serverUrl
+
+rm $tmpFile
+
+# You must run this via: . ./devStart
+# this allows the file to properly export serverUrl for test.
+
+# then npm start to run the server
+# make sure https://developers.facebook.com/apps/1291586900892998/webhooks/ are properly set.
