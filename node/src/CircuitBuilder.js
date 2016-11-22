0424feb74fe43173247f01bf98f96b522a250921
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-11-21 17:37:16 -0800

    add back, cardio, chest, fullbody, lowerbody exercises

--- node/src/CircuitBuilder.js
+++ node/src/CircuitBuilder.js
@@ -20,11 +20,12 @@ export default class CircuitBuilder {
     this.numRounds = 1;
     this.numExercises = 1;
     this.name = '';
+    this.followedByCircuit = undefined;
   }
 
   withNumRounds(numRounds) {
     this.numRounds = numRounds;
-    return this;[]
+    return this;
   }
 
   withNumExercises(numExercises) {
@@ -37,13 +38,22 @@ export default class CircuitBuilder {
     return this;
   }
 
+  withFollowingCircuit(circuit) {
+    this.followedByCircuit = circuit;
+    return this;
+  }
+
   build() {
     if (this.exerciseList.length < this.numExercises) {
       throw Error(`Tried to build circuit with ${this.numExercises} from list with`
         + ` only ${this.exerciseList.length} options.`);
     }
 
-    const exercisesInCurcuit = pickRandomFromList(this.exerciseList, this.numExercises);
+    let exercisesInCurcuit = pickRandomFromList(this.exerciseList, this.numExercises);
+
+    if (this.followedByCircuit) {
+      exercisesInCurcuit = exercisesInCurcuit.concat(this.followedByCircuit.exercises);
+    }
 
     return {
       name: this.name,
--- node/src/workouts.js
+++ node/src/workouts.js
@@ -2,90 +2,62 @@ import CircuitBuilder from './CircuitBuilder';
 import WorkoutState from './WorkoutState';
 import { loadWorkoutByOwner, storeWorkoutByOwner, deleteWorkoutByOwner } from './WorkoutConnector';
 
-// Pick 2, 3 rounds
-const compoundOptions = [
-  'Static lunge + one arm cable row - 15 reps per arm',
-  'Static lunge + 2 arm cable row - 15 reps',
-  'Squat + row - 10 reps',
-  'Walking lunge + neutral grip dumbbell curl + neutral grip shoulder press - 20 steps/presses',
-  'Wall sit + lateral dumbbell shoulder raise - 10 reps for the raise + 30 second additional wall sit with weight',
-  'Kettlebell deadlift + low row/chin raise - 15 reps',
-  'Thrusters (half squat + neutral grip dumbbell press) - 20 reps'
-];
-
-// Pick 1, 3 rounds. If do this, skip compound.
-const fullBodyOptions = [
-  'Barbell back squat - 12, 10, 8 reps',
-  'Hex bar or barbell deadlift - 8, 6, 4 reps',
-];
-
-// Pick 3, 2 rounds
-const lowerBodyOptions = [
-  'trx one leg squats - 10 reps each leg',
-  'trx two leg squats - 20 reps',
-  'trx jump squats - 15 reps',
-  'trx hamstring press (heels in straps) - 10 reps',
-  'leg press machine - 15 reps',
-  'heavy dumbbell walking lunges - 16 steps',
-  'kettlebell front squat - 15 reps',
-  'box jumps - 12 jumps',
-  'hamstring curl machine - 8 reps',
-  'jumping lunges - 40 reps',
-  'one leg dumbbell toe touch - 10 reps each leg',
-];
-
-// Pick 2, 2 rounds.
-const backOptions = [
-  'kneeling dumbbell row  - 8 reps',
-  'rear deltoid/back fly machine - 12 reps',
-  'pull ups (use assisted machine if needed and available) - 8 reps',
-  'chin ups (use assisted machine if needed and available) - 8 reps',
-  'lat pull down machine - 10 reps',
-  'trx rows - 15 reps',
-  'standing barbell high pull - 8 reps',
-  'static squat + 2 arm row - 20 reps',
-  'seated machine rows - 12 reps'
-];
-
-// Pick 2, 2 rounds.
-const chestOptions = [
-  'neutral grip dumbell chest press on swiss ball - 12 reps',
-  'conventional grip dumbell chest press on swiss ball- 10 reps',
-  'Alternating dumbell chest press on swiss ball (either grip) - 16 reps (8 per arm, hold one arm up while pressing with the other',
-  'dumbell chest fly on swiss ball - 10 reps',
-  'barbell bench press - 5-8 reps',
-  'push ups - 15 reps',
-  'tricep isolation push ups - 20 reps',
-  'push ups with feet elevated - 15 reps',
-  'machine chest flys - 10 reps',
-];
-
-const cardioOptions = [
-  'jump rope - 120 singles',
-  'bike - seated 30 seconds, standing 30 seconds - 3 minutes',
-  '3 minute 5% incline treadmill run',
-  '15 burpees',
-  '3 minutes high intensity elliptical machine - 80% effort',
-  '1/3 mile run',
-];
-
 const coreExercises = require('../exercises/core.json');
 const compoundExercises = require('../exercises/compound.json');
+const fullBodyExercises = require('../exercises/fullBody.json');
+const lowerBodyExercises = require('../exercises/lowerBody.json');
+const backExercises = require('../exercises/back.json');
+const chestExercises = require('../exercises/chest.json');
+const cardioExercises = require('../exercises/cardio.json');
 
 function buildFreshWorkout() {
   const coreCircuit = new CircuitBuilder(coreExercises)
     .withNumExercises(6)
     .withNumRounds(2)
     .withName('Core')
+    .withFollowingCircuit(new CircuitBuilder(cardioExercises).withNumExercises(1).build())
     .build();
 
   const compoundCircuit = new CircuitBuilder(compoundExercises)
     .withNumExercises(2)
     .withNumRounds(3)
     .withName('Compound')
+    .withFollowingCircuit(new CircuitBuilder(cardioExercises).withNumExercises(1).build())
     .build();
 
-  return new WorkoutState([coreCircuit, compoundCircuit]);
+  const fullBodyCircuit = new CircuitBuilder(fullBodyExercises)
+    .withNumExercises(1)
+    .withNumRounds(3)
+    .withName('Full-Body')
+    .withFollowingCircuit(new CircuitBuilder(cardioExercises).withNumExercises(1).build())
+    .build();
+
+  const compoundOrFullBody = Math.random() > 0.5 ? compoundCircuit : fullBodyCircuit;
+
+  const lowerBodyCircuit = new CircuitBuilder(lowerBodyExercises)
+    .withNumExercises(3)
+    .withNumRounds(2)
+    .withName('Lower-Body')
+    .build();
+
+  const backCircuit = new CircuitBuilder(backExercises)
+    .withNumExercises(2)
+    .withNumRounds(2)
+    .withName('Back')
+    .build();
+
+  const chestCircuit = new CircuitBuilder(chestExercises)
+    .withNumExercises(2)
+    .withNumRounds(2)
+    .withName('Chest')
+    .build();
+
+  return new WorkoutState([
+    coreCircuit,
+    compoundOrFullBody,
+    lowerBodyCircuit,
+    chestCircuit,
+    backCircuit]);
 }
 
 export function clearWorkout(userId) {
